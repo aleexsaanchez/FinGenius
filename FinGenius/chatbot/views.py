2800560fde4e84bfc9b136ad2885e9297876bb02
aleexsaanchez <alejandrosanchezsayes2003@gmail.com>
2024-11-01 17:04:17 -0400

    Version 0.4
    Added the OPENAI API
    Updated the chat view, now users can chat with the bot
    Coming next ->
    Authenticated Users Only Chat
    Save Entries with the User ID

--- FinGenius/chatbot/views.py
+++ FinGenius/chatbot/views.py
@@ -1,14 +1,32 @@
+import openai
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+openai.api_key = "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"
 
 def chatview(request):
-    if request.method == 'POST':
-        # Handle POST request
-        # Process user input, get response from chatbot, etc.
-        return HttpResponse("Chat response")
-    else:
-        # Handle GET request
-        return render(request, 'chatbot/chat.html')
+    if request.method == "POST":
+        user_input = request.POST.get('user_input', '')
+
+        #check if the users input exists
+        if user_input:
+            #call Chatgpt openai 
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages = [
+                    {"role": "system", "content": "You are a helpful financial advice chatbot."},
+                    {"role": "user", "content": user_input}
+                ]
+            )
+
+            #extract the bot's response
+            bot_response = response['choices'][0]['message']['content'].strip()
+
+            #return response as JSON
+            return JsonResponse({'response': bot_response})
+
+    #render the chat template for get requests    
+    return render(request, 'chatbot/chat.html')
 
 def feedbackview(request):
     if request.method == 'POST':
